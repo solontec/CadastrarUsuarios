3c805b4c59748e21fae04fd3eebf8bde7b384bf2
gui <guisolon.tec@gmail.com>
2025-07-12 16:17:12 -0300

    Minha primeira aplicação

---
+++ src/main/java/Java10x/CadastrarUsuarios/CadastrarUsuariosApplication.java
@@ -0,0 +1,13 @@
+package Java10x.CadastrarUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrarUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrarUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Java10x/CadastrarUsuarios/CadastrarUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package Java10x.CadastrarUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrarUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
