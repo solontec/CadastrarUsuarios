449879abd0be872b59fd192553ffc8ae46e2a6cd
gui <guisolon.tec@gmail.com>
2025-07-12 16:26:58 -0300

    first commit

--- src/main/java/Java10x/CadastrarUsuarios/CadastrarUsuariosApplication.java
+++ src/main/java/Java10x/CadastrarUsuarios/CadastrarUsuariosApplication.java
@@ -2,7 +2,7 @@ package Java10x.CadastrarUsuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// oi
 @SpringBootApplication
 public class CadastrarUsuariosApplication {
 
